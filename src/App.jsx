b2be9f7e5b152b61b33c2e873f7782de15c23486
Deshan Jayawardhana <lahirujayawardana6@gmail.com>
2025-09-16 15:35:22 +0530

    Adding some front-end elements and styled them using tailwind CSS
    feat: integrate Tailwind CSS and refactor App component
    
    - Added Tailwind CSS and related dependencies to package.json.
    - Removed App.css and migrated styles to Tailwind CSS classes in App.jsx.
    - Created a Calculator component with button styling using Tailwind.
    - Updated index.css to import Tailwind CSS.
    - Configured Vite to use Tailwind CSS plugin.
    - Add tailwind-merge package.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { twMerge } from 'tailwind-merge';
+
+function CalButtons({ children, className }) {
+  return (
+    <button className={twMerge('bg-white aspect-square rounded-2xl py-3 shadow-sm text-gray-600 hover:brightness-90 transition-all duration-200 cursor-pointer active:brightness-80 active:text-gray-900 active:text-2xl text-3xl', className)}>
+      {children}
+    </button>
+  );
+}
+
+
+function Calculator() {
+  return (
+    <div className='bg-gray-400 flex items-center justify-center gap-3 rounded-2xl p-5 '>
+      <div className='grid grid-cols-4 gap-5'>
+        <CalButtons className='bg-red-300'>C</CalButtons>
+        <CalButtons>%</CalButtons>
+        <CalButtons>√</CalButtons>
+        <CalButtons>÷</CalButtons>
+        <CalButtons>7</CalButtons>
+        <CalButtons>8</CalButtons>
+        <CalButtons>9</CalButtons>
+        <CalButtons>×</CalButtons>
+        <CalButtons>4</CalButtons>
+        <CalButtons>5</CalButtons>
+        <CalButtons>6</CalButtons>
+        <CalButtons>-</CalButtons>
+        <CalButtons>1</CalButtons>
+        <CalButtons>2</CalButtons>
+        <CalButtons>3</CalButtons>
+        <CalButtons>+</CalButtons>
+        <CalButtons>0</CalButtons>
+        <CalButtons>.</CalButtons>
+        <CalButtons>=</CalButtons>
+      </div>
+    </div>
+  )
+
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    <div className="text-center font-bold bg-linear-to-br from-slate-700 to-slate-950 min-h-screen flex justify-center items-center">
+      <Calculator />
+    </div>
+
+
   )
 }
 
